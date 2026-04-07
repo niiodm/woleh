c777ede6e2fd1a2cbb241bb02ad65027772b293c
morgan <williammorgandarko@gmail.com>
2026-04-07 22:29:52 +0000

    fix android ndk mismatch

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "odm.clarity.woleh_mobile"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
